f36624ae9b20906062b11aa8d3ff4726089d72c4
Louisss-Lam <louislam617@gmail.com>
2022-09-21 20:44:55 +0100

    solution 2

--- src/Solution.java
+++ src/Solution.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Solution {
 
 	public static void main(String[] args) {
-		int[] nums = {1, 2, 3, 4};
+		int[] nums = {3, 2, 3, 4};
 		System.out.println(Arrays.toString(productExceptSelf(nums)));
 
 	}
@@ -11,25 +11,17 @@ public class Solution {
 	public static int[] productExceptSelf(int[] nums) {
 		
 		int len = nums.length;
-		int[] answer = new int[len];
-		int[] left = new int[len];
-		int[] right = new int[len];
-		
-		// fill the left array
-		left[0] = 1;
+		int[] answer = new int[len];		
+
+		answer[0] = 1;
 		for (int i = 1; i < len; i++) {
-			left[i] = left[i-1] * nums[i-1];
+			answer[i] = answer[i-1] * nums[i-1];
 		}
 		
-		// fill the right array 
-		right[len-1] = 1;
-		for (int i = len-2; i >= 0; i--) {
-			right[i] = right[i+1] * nums[i+1];
-		}
-		
-		// fill the answer array
-		for (int i = 0; i < len; i++) {
-			answer[i] = left[i] * right[i];
+		int right = 1;
+		for (int i = len - 1; i >= 0; i--) {
+			answer[i] =	answer[i] * right;
+			right = right * nums[i];
 		}
 		
 		return answer;
